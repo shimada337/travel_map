1d06dd2b0f3ab329310fabba53df266477254553
shimada337 <eisukeqoo@icloud.com>
2022-06-17 08:46:58 +0000

    [add]マップ表示

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "cb73fcd9a85e44b7acd7fb4dfed26990.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
