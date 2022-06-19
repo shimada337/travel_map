307104cb7a06d447e098add80e541d6f59db9bb2
shimada337 <eisukeqoo@icloud.com>
2022-06-19 13:01:15 +0000

    [add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'maps#index'
   resources :maps, only: [:index]
