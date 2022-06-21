c585dc51ed65873a008e1ebdf380126f601e653e
shimada337 <eisukeqoo@icloud.com>
2022-06-21 04:41:25 +0000

    [add]ユーザー更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users, controllers: {
     registrations: "user/registrations",
     sessions: 'user/sessions'
