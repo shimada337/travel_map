33fcc5269b023956f8cad91f0629eeadfda30b38
shimada337 <eisukeqoo@icloud.com>
2022-06-26 08:39:37 +0000

    [update]都道府県、タグ検索

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
   end
   
   def search
-    @results = @q.result
+    @results = Post.ransack(params[:q]).result(distinct: true)
   end
   
   private
