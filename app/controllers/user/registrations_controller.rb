8a710124d90d8200aa2755f174a2a2e089f24329
shimada337 <eisukeqoo@icloud.com>
2022-06-26 13:53:17 +0000

    [add]サインイン

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -3,6 +3,16 @@
 class User::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+  
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   # GET /resource/sign_up
   # def new
