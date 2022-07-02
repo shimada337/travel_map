5a5ca8a8255da0176c496f242027e8197e8f1000
shimada337 <eisukeqoo@icloud.com>
2022-07-02 15:41:59 +0000

    [add]ユーザーバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :posts
+  
+  validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :email, presence: true
 end
