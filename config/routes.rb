3d760fe96d7fb69aad2fc31fe46e1e342856ab02
shimada337 <eisukeqoo@icloud.com>
2022-07-24 15:32:16 +0000

    [add]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,9 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
+  resources :contacts, only: [:new, :create]
+  post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
+  post 'contacts/back', to: 'contacts#back', as: 'back'
+  get 'done', to: 'contacts#done', as: 'done'
   resources :maps, only: [:index]
 end
